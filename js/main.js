698ae279cbf3b772ec50edf397f0a13247f66322
Your Name <pasha.rodoslavov@gmail.com>
2018-05-31 14:37:03 -0700

    test3

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ let count = 0;
 const keyrow = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 const ul = document.querySelector('.section ul');
+let phraseInDisplay = document.getElementsByClassName('letter');
+const img = document.querySelectorAll('img');
 const arrPhrases = [
 	'The Shawshank Redemption',
 	'The Godfather',
@@ -15,17 +17,14 @@ const arrPhrases = [
 	'Pulp Fiction'
 ];	
 
-// Getting random phrase from arr
+// Getting random phrase from array
 
-const randomPhrases = arrPhrases[Math.floor(Math.random() * arrPhrases.length)];
-const splitedPhrase = randomPhrases.split('');
+const splitedPhrase = arrPhrases[Math.floor(Math.random() * arrPhrases.length)].split('');
 
 // Adding random phrase to display by creating li
 
-
 function addPhraseToDisplay(item){
 	let li = document.createElement('LI');
-
 	for (let i = 0; i < item.length; i++){
 		let li = document.createElement('LI');		
 		li.textContent = item[i];
@@ -38,58 +37,13 @@ function addPhraseToDisplay(item){
 	}	
 }
 
+// Applying styles after finish game
 
-function presKey(keyrow){   // Main function
-
-
-	// Adds event to every buttons
-
-	keyrow.addEventListener('click', (e)=>{	
-		let letterFound = 0;
-		let liInPrase = phrase.querySelectorAll('.letter');	
-		let img = document.querySelectorAll('img');
-		if (e.target.tagName == "BUTTON"){			
-			e.target.disabled = true;				
-			for (var i = 0; i < liInPrase.length; i++){	
-
-	// Checks matching guess and show wrong or right		
-
-				if (e.target.textContent.toLowerCase() == liInPrase[i].textContent.toLowerCase()){
-					liInPrase[i].className += ' show';
-					e.target.className = 'right';
-					letterFound = e.target.textContent;
-				}								
-				else if(e.target.className !=='right'){
-					e.target.className = 'wrong';					
-				}	
-			}	
-
-	// Checks user won or lost
-
-			if (phrase.querySelectorAll('.show').length ==	liInPrase.length){
-					overlay.style.display = "flex";
-					overlay.className = 'win';
-					start.textContent = 'Start Again'
-				}
-			else if (count == img.length) {
-				overlay.style.display = "flex";
-				overlay.className = 'lose';
-				start.textContent = 'Start Again'
-			}
-
-	// Shows quantity tries
-
-			else if (!letterFound && count <= img.length){
-				const img = document.getElementsByTagName('img');
-				count++;
-  				img[count - 1].src = 'images/lostHeart.png'; 
-			}		
-		}	
-	});
-
-};
-
-
+function stylesAfterFinish (){
+	overlay.style.display = "flex";					
+	start.textContent = 'Start Again';
+	phrase.style.display = "none";
+}
 
 // Starts and restarts game
 
@@ -99,11 +53,47 @@ start.addEventListener('click', (e) => {
 		}
 		else{
 			overlay.style.display = 'none';
-			addPhraseToDisplay(splitedPhrase);
-			presKey(keyrow);
+			addPhraseToDisplay(splitedPhrase);			
 		}
 	});
-
 	
+// Adds event to every buttons key
 
-	
+keyrow.addEventListener('click', (e)=>{	
+	let letterFound = 0	
+	if (e.target.tagName == "BUTTON"){		// If button - go
+		e.target.disabled = true;				
+		for (var i = 0; i < phraseInDisplay.length; i++){	
+
+// Checks matching guess and shows wrong or right 
+
+			if (e.target.textContent.toLowerCase() == phraseInDisplay[i].textContent.toLowerCase()){
+				phraseInDisplay[i].className += ' show';
+				e.target.className = 'right';
+				letterFound = e.target.textContent;
+			}								
+			else if(e.target.className !=='right'){
+				e.target.className = 'wrong';					
+			}	
+		}	
+
+// Shows quantity tries
+
+		if (!letterFound && count <= img.length){				
+			count++;
+				img[count - 1].src = 'images/lostHeart.png'; 
+		}
+
+// Checks user won or lost
+
+		if (phrase.querySelectorAll('.show').length ==	phraseInDisplay.length){
+				overlay.className = 'win';
+				stylesAfterFinish ();		// Applying styles				
+			}
+		else if (count == img.length) {
+		 	overlay.className = 'lose';
+			stylesAfterFinish ();		// Applying styles 		
+		}		
+	}	
+});
+
