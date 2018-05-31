80437f52fc123e483fc3042082b1be5be701a38a
Your Name <pasha.rodoslavov@gmail.com>
2018-05-30 22:36:12 -0700

    test

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
+// Variables
+
 const start = document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
 let count = 0;
 const keyrow = document.querySelector('#qwerty');
-// const button = keyrow.querySelectorAll('button');
-// console.log(button);
 const phrase = document.querySelector('#phrase');
 const ul = document.querySelector('.section ul');
 const arrPhrases = [
@@ -14,11 +14,16 @@ const arrPhrases = [
 	'The Lord of the Ring',
 	'Pulp Fiction'
 ];	
+
+// Getting random phrase from arr
+
 const randomPhrases = arrPhrases[Math.floor(Math.random() * arrPhrases.length)];
 const splitedPhrase = randomPhrases.split('');
 
+// Adding random phrase to display by creating li
 
-function addPhraseToDispla(item){
+
+function addPhraseToDisplay(item){
 	let li = document.createElement('LI');
 
 	for (let i = 0; i < item.length; i++){
@@ -34,71 +39,71 @@ function addPhraseToDispla(item){
 }
 
 
-function presKey(keyrow){
+function presKey(keyrow){   // Main function
+
+
+	// Adds event to every buttons
+
 	keyrow.addEventListener('click', (e)=>{	
-		let img = document.querySelectorAll('img');		
+		let letterFound = 0;
+		let liInPrase = phrase.querySelectorAll('.letter');	
+		let img = document.querySelectorAll('img');
 		if (e.target.tagName == "BUTTON"){			
-			e.target.disabled = true;
-			let letterFound = 0;
-			let liInPrase = phrase.querySelectorAll('.letter');		
-				for (var i = 0; i < liInPrase.length; i++){				
-					if (e.target.textContent.toLowerCase() == liInPrase[i].textContent.toLowerCase()){
-						liInPrase[i].className += ' show';
-						e.target.className = 'right';
-						letterFound = e.target.textContent;
-	      				console.log(letterFound);	
-					}								
-					else if(e.target.className !=='right'){
-						e.target.className = 'wrong';					
-					}				
+			e.target.disabled = true;				
+			for (var i = 0; i < liInPrase.length; i++){	
+
+	// Checks matching guess and show wrong or right		
+
+				if (e.target.textContent.toLowerCase() == liInPrase[i].textContent.toLowerCase()){
+					liInPrase[i].className += ' show';
+					e.target.className = 'right';
+					letterFound = e.target.textContent;
+				}								
+				else if(e.target.className !=='right'){
+					e.target.className = 'wrong';					
 				}	
-				if (count == img.length) {
+			}	
+
+	// Checks user won or lost
+
+			if (phrase.querySelectorAll('.show').length ==	liInPrase.length){
 					overlay.style.display = "flex";
-					overlay.className = 'lose';
+					overlay.className = 'win';
 					start.textContent = 'Start Again'
 				}
-				else if (!letterFound && count <= img.length){
-					const img = document.getElementsByTagName('img');
-					count++;
-	  				img[count - 1].src = 'images/lostHeart.png'; 
-				}			
+			else if (count == img.length) {
+				overlay.style.display = "flex";
+				overlay.className = 'lose';
+				start.textContent = 'Start Again'
+			}
+
+	// Shows quantity tries
+
+			else if (!letterFound && count <= img.length){
+				const img = document.getElementsByTagName('img');
+				count++;
+  				img[count - 1].src = 'images/lostHeart.png'; 
+			}		
 		}	
 	});
+
 };
 
 
 
+// Starts and restarts game
 
-
-	start.addEventListener('click', () => {
+start.addEventListener('click', (e) => {
 		if (start.textContent == 'Start Again'){
 			document.location.reload(true);		
 		}
 		else{
 			overlay.style.display = 'none';
-			addPhraseToDispla(splitedPhrase);
+			addPhraseToDisplay(splitedPhrase);
 			presKey(keyrow);
-		}		
-
+		}
 	});
 
-	// 	start.addEventListener('touchend', () => {
-	// 	if (start.textContent == 'Start Again'){
-	// 		document.location.reload(true);		
-	// 	}
-	// 	else{
-	// 		overlay.style.display = 'none';
-	// 		addPhraseToDispla(splitedPhrase);
-	// 		presKey(keyrow);
-	// 	}		
+	
 
-	// });
-
-
-
-
-// 	$('.btn__reset').on('click', (e)=>{
-// 	$('#overlay').hide();
-// 	addPhraseToDispla(splitedPhrase);
-// 	presKey(keyrow);
-// });
+	
